9db61952ff8f40052a226334895b1039c4ef9449
Josephine Wang <josiexw@dhcp-10-31-189-41.dyn.MIT.EDU>
2025-11-07 14:33:13 -0500

    bashscript

---
+++ main.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#SBATCH -p sched_mit_kburdge_r8      # partition name
+#SBATCH --job-name=myjob             # name for your job
+#SBATCH --gres=gpu:1                 # if you need GPUs
+#SBATCH --ntasks=1                   # number of tasks (often 1 for serial jobs)
+#SBATCH --cpus-per-task=4            # CPU cores per task
+#SBATCH --mem=16G                    # memory per node
+#SBATCH --time=02:00:00              # max walltime (HH:MM:SS)
+#SBATCH --output=slurm-%j.out        # output file (%j = job ID) to capture logs for debugging
+
+# # Load your shell environment to activate your Conda environment
+# source /home/user/.bashrc
+# conda activate myconda
+VENV_PATH="/path/to/your/venv" 
+
+# Check if the virtual environment exists
+if [ -d "venv" ]; then
+    echo "Activating virtual environment at $VENV_PATH..."
+    source "$VENV_PATH/bin/activate"
+else
+    echo "Error: Virtual environment not found at $VENV_PATH"
+    exit 1
+fi
+
+# Load any modules or software you need
+module load cuda/12.0
+
+# Run your command or script
+python src/models/ddpm.py
